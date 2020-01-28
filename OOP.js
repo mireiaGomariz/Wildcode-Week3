892a041dd1d0092bdd898a2b92f66f952f0020b9
mireiaGomariz <mireiapgomariz@gmail.com>
2020-01-28 10:57:45 +0100

    OOP

---
+++ OOP.js
@@ -0,0 +1,34 @@
+// let person = {
+//   name: 'Mireia',
+//   city: 'Sabadell',
+//   age: '26',
+//   sayPresentation: function() {
+//     return "hello " + this.name;
+//   }
+// }
+//
+// console.log(person.sayPresentation())
+
+// NOT DO IT!!
+// let person = {
+//   name: 'Mireia',
+//   city: 'Sabadell',
+//   age: '26',
+//   sayPresentation: () => { `hello ${this.name}`}
+// }
+//
+// console.log(person.sayPresentation())
+
+
+let Person = function (firstName, city) {
+  this.firstName = firstName;
+  this.city = city;
+
+  this.sayHello = function () {
+    console.log(`My name is ${this.firstName}`);
+  }
+}
+
+const marc = new Person('Marc', 'Barcelona');
+
+marc.sayHello();
